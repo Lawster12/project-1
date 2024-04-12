93aa479932f7fc4592ea8cb21b2c8258cb2dfb0b
Cole <cole.patter@gmail.com>
2024-04-11 19:18:05 -0500

    musicBrainz cleanup & object definition

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
 const searchModalRootEl = $('#search-modal-container')
 const searchModalCancelBtn = $('#search-modal-cancel')
+const searchModalSearchBtn = $('#search-modal')
+const searchModalInputEl = $('#search-input')
+
+
 
 function closeModal() {
     searchModalRootEl.css('display', 'none')
@@ -18,5 +22,17 @@ function toggleModal() {
     }
 }
 
+function search() {
+    const queryValue = searchModalInputEl.val()
+
+    spotify.searchForTrack(queryValue).then(function (spotifyData) {
+        console.log(spotifyData)
+        getMusicBrainzArtistData(spotifyData.artists[0].name).then(function (musicBrainzData) {
+            console.log(musicBrainzData)
+        })
+    })
+}
+
 $('#new-search').on("click", toggleModal)
 searchModalCancelBtn.on("click", closeModal)
+searchModalSearchBtn.on('click', search)

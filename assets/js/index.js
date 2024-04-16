e9cf5e3a8e36d293da0c03cb8ef7865efcacd031
Uriel Blanco <uriel.techgzz@gmail.com>
2024-04-15 19:22:38 -0500

    added scrolling effect to page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,23 @@ const searchModalRootEl = $('#search-modal-container')
 const searchModalCancelBtn = $('#search-modal-cancel')
 const searchModalSearchBtn = $('#search-modal')
 const searchModalInputEl = $('#search-input')
+const topTenContainer = $('#topTenContainer') //added this variable
 const searchModalLoadingSpinner = $('.search-modal-loading-spinner')
 const searchResultEl =  $("#searchResult")
+
+
+//added scroller ribbon
+const track1 =  $("#track1") // added variable for each track 
+const track2 =  $("#track2")
+const track3 =  $("#track3")
+const track4 =  $("#track4")
+const track5 =  $("#track5")
+const track6 =  $("#track6")
+const track7 =  $("#track7")
+const track8 =  $("#track8")
+const track9 =  $("#track9")
+const track10 =  $("#track10")
+
 
 const areaofOriginEl = $("<p>")
 const yearEstablishedEl = $("<p>")
@@ -23,6 +38,7 @@ const songAlbumArtDisplayEl = $('.song-album-art-display')
 
 function closeModal() {
     searchModalRootEl.css('display', 'none')
+    
 }
 
 function toggleModal() {
@@ -31,7 +47,7 @@ function toggleModal() {
     
     if (elDisplay === 'flex') {
         searchModalRootEl.css('display', 'none')
-    }
+    } 
     
     if (elDisplay === 'none') {
         searchModalRootEl.css('display', 'flex')
@@ -41,15 +57,12 @@ function toggleModal() {
 function search() {
     // show loading spinner
     searchModalLoadingSpinner.css('display', 'block')
-
     const queryValue = searchModalInputEl.val()
     spotify.searchForTrack(queryValue).then(function (spotifyData) {
         console.log(spotifyData)
         getMusicBrainzArtistData(spotifyData.artistName).then(function (musicBrainzData) {
-            closeModal()
             searchModalLoadingSpinner.css('display', 'none')
             searchModalInputEl.val('')
-
             // all network fetching is done, do stuff here
             songTitleDisplayEl.text(spotifyData.trackName)
             songAlbumArtDisplayEl.attr('src', spotifyData.albumArt)
@@ -83,9 +96,36 @@ function search() {
             externalUrlEl.text("Open in Spotify")
             externalUrlEl.attr("href", spotifyData.externalURL)
             searchResultEl.append(externalUrlEl)
+
+
+            // getTopTen(spotifyData.artistid) returns info from spotify
+            spotify.getTopTen(spotifyData.artistid).then(function (trackstuff) {
+                console.log(trackStuff);
+                
+                track1.text(trackStuff.track1);
+                track2.text(trackStuff.track2);
+                track3.text(trackStuff.track3);
+                track4.text(trackStuff.track4);
+                track5.text(trackStuff.track5);
+                track6.text(trackStuff.track6);
+                track7.text(trackStuff.track7);
+                track8.text(trackStuff.track8);
+                track9.text(trackStuff.track9);
+                track10.text(trackStuff.track10);
+                topTenContainer.css('display', 'block')
+                })
+
+
+
+
+
+
+
         })
     })
 }
+
+
 
 $('#new-search').on("click", toggleModal)
 searchModalCancelBtn.on("click", closeModal)
@@ -93,4 +133,6 @@ searchModalCancelBtn.on("click", closeModal)
 $('#clear').click(() => $('.history').css('display', 'none'));
 
 searchModalSearchBtn.on('click', search)
+
+
 
